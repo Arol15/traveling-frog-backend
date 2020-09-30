cbbbb0ba7ba65150789b11846e3931576d111aa2
Lora Rusinouskaya <lrusinou@MacBook-Pro.local>
2020-09-30 13:59:45 -0400

    updated variables in config file

--- app/config.py
+++ app/config.py
@@ -4,6 +4,6 @@ class Configuration:
     SQLALCHEMY_DATABASE_URI=os.environ.get("DATABASE_URL") or "postgresql://travel_app_user:travel@localhost/travel_app_db"
     SQLALCHEMY_TRACK_MODIFICATIONS=False
     SECRET_KEY=os.environ.get("SECRET_KEY")
-    S3_BUCKET=os.environ.get("S3_BUCKET")
-    S3_KEY=os.environ.get("S3_KEY")
-    S3_SECRET_ACCESS_KEY=os.environ.get("S3_SECRET_ACCESS_KEY")
+    S3_BUCKET_NAME=os.environ.get("S3_BUCKET_NAME")
+    AWS_ACCESS_KEY_ID=os.environ.get("AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY=os.environ.get("AWS_SECRET_ACCESS_KEY")
